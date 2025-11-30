d476bc97ccff75b64bef6a7a406333d52c00a2a3
Afonso Gaspar de Oliveira Sítima <afonso.sitima@tecnico.ulisboa.pt>
2025-11-30 19:05:16 +0000

    feito

--- projeto/projeto.cpp
+++ projeto/projeto.cpp
@@ -8,12 +8,19 @@
 struct Aminoacid {
     char type;
     long long energy;
+    int index;
+};
+
+struct KeepTrack {
+    std::string numList;
+    std::string allTypes;
+    long long totalEnergy = 0;
 };
 
 int affinity(char aa1, char aa2){
-    std::map<std::pair<char, char>, int> affinity_map = {
+    static std::map<std::pair<char, char>, int> affinity_map = {
         {{'P', 'P'},  1}, {{'P', 'N'}, 3}, {{'P', 'A'}, 1}, {{'P', 'B'}, 3},
-        {{'N', 'P'}, 5}, {{'N', 'N'},  1}, {{'N', 'A'}, 0}, {{'N', 'B'}, -2},
+        {{'N', 'P'}, 5}, {{'N', 'N'},  1}, {{'N', 'A'}, 0}, {{'N', 'B'}, 1},
         {{'A', 'P'}, 0}, {{'A', 'N'}, 1}, {{'A', 'A'},  0}, {{'A', 'B'},  4},
         {{'B', 'P'}, 1}, {{'B', 'N'}, 3}, {{'B', 'A'},  2}, {{'B', 'B'},  3}
     };
@@ -30,7 +37,7 @@ long long removal_energy(Aminoacid aa1, Aminoacid aa2, Aminoacid aa3){
     return aa1.energy *affinity(aa1.type,aa2.type)*aa2.energy + aa2.energy * affinity(aa2.type,aa3.type)*aa3.energy;
 }
 
-int best_removal_energy(std::vector<Aminoacid> chain, int position, int beg, int end){
+/*int best_removal_energy(std::vector<Aminoacid> chain, int position, int beg, int end){
     Aminoacid terminal;
     terminal.type = 'T';
     terminal.energy = 1;
@@ -44,16 +51,74 @@ int best_removal_energy(std::vector<Aminoacid> chain, int position, int beg, int
     return max(best_removal_energy(chain,,beg,position))
 }
 
+*/
+std::string mergeLexico(const std::string &a, const std::string &b) {
+    std::string res;
+    size_t i = 0, j = 0;
+
+    while (i < a.size() && j < b.size()) {
+        // comparamos os sufixos a partir de i e j
+        if (a.compare(i, std::string::npos, b, j, std::string::npos) <= 0) {
+            res.push_back(a[i]);
+            i++;
+        } else {
+            res.push_back(b[j]);
+            j++;
+        }
+    }
+
+    // copia o que sobrar de uma das duas
+    res.append(a.begin() + i, a.end());
+    res.append(b.begin() + j, b.end());
+
+    return res;
+}
+
+void changeMax(KeepTrack* n1, KeepTrack* max) {
+    if (n1->totalEnergy > max->totalEnergy) *max = *n1;
+    else if (n1->totalEnergy == max->totalEnergy &&   n1->allTypes < max->allTypes)  *max = *n1; //se não passar a testes mudar isto O(n)
+    return;
+}
+
+
+bool ab_less_ba(std::string &a, std::string &b) {
+    size_t n = a.size();
+    size_t m = b.size();
+    size_t total = n + m;
+    for (size_t k = 0; k < total; ++k) {
+        char c1 = (k < n) ? a[k] : b[k - n];
+        char c2 = (k < m) ? b[k] : a[k - m];
+        if (c1 < c2) return true; //(a + b) < (b + a)
+        if (c1 > c2) return false; //(a + b) > (b + a)
+    }
+    return false; // (a+b) == (b+a)
+}
+
+void addSubProblems(KeepTrack* n1, KeepTrack* n2, Aminoacid aa, KeepTrack* result) {
+    result->totalEnergy = n1->totalEnergy + n2->totalEnergy + aa.energy;
+    if(n1->allTypes + n2->allTypes <= n2->allTypes + n1->allTypes) {   //caso for lento trocar
+        result->allTypes = n1->allTypes + n2->allTypes;   
+        result->numList = n1->numList + n2->numList;
+    }
+    else{
+        result->allTypes = n2->allTypes + n1->allTypes;
+        result->numList = n2->numList + n1->numList;
+    }
+    result->allTypes += aa.type;
+    result->numList += std::to_string(aa.index) + " ";
+    return; 
+}
+
+
 
 
 int main(){
     int n;
-    int size;
-    long long max_energy[n][n];
     scanf("%d", &n);
 
-    Aminoacid chain[n + 2];
-    char chain_types[n + 2];
+    std::vector<std::vector<KeepTrack>> max_energy(n + 2, std::vector<KeepTrack>(n + 2));
+    std::vector<Aminoacid> chain(n + 2);
+    std::vector<char> chain_types(n + 2);
 
     chain[0].type = 'T';
     chain[0].energy = 1;
@@ -61,61 +126,37 @@ int main(){
     chain[n + 1].energy = 1;
     
     for (int i = 1; i < n + 1; i++)
-        scanf("%d", &chain[i].energy);
+        scanf("%lld", &chain[i].energy);
 
-    scanf("%s", chain_types);
+    scanf("%s", chain_types.data());
     for (int i = 0; i < n; i++)
-        chain[i + 1].energy = chain_types[i];
+        chain[i + 1].type = chain_types[i];
 
+    //isto está a fazer o tamanho da diagonal
+    for (int len = 1; len <= n; len++) {  //tamanho da diagonal
+        
 
-    for (int i = 0; i < n; i++) {
+        for(int i = 1; i + len - 1 <= n; i++) {  //i é o index horizontal e j index vertical
+            int j = len + i - 1;   //mexer na diagonal  j - i + 1 = len
+            
 
-        for(int j = 0; j < n - i; j++) {
-            size = i - j +1;
-            if (size<=0) continue;
-            long long energy = 0;
-            for (int k = i; k <= n-size; k++) {
-                if(max_energy[k][k+size-1] != NULL){
-                    energy += max_energy[k][k+size-1];
-                }
-                else if (size == 1){
-                    energy += removal_energy(chain[k], chain[k+1], chain[k + 2]);
-                    max_energy[k][k+size-1] = energy;
-                }
-                else{
-                    //falta calcular para os casos à direita
-                }
+            for(int k = i; k <= j; k++) {  //mexe na horizontal (escolhe qual é o ultimo a sair)
+                Aminoacid aa;
+                KeepTrack subComplete;
+                aa.energy = removal_energy(chain[i - 1], chain[k], chain[j + 1]); //k é a ultima energia a ser removida
+                aa.type = chain[k].type;
+                aa.index = k;
+                
+                KeepTrack sub1 = max_energy[i][k - 1];  //temos que ver qual é lexicalmente menor entre estas duas
+                KeepTrack sub2 = max_energy[k + 1][j];
 
-
-                //max();
-
+                addSubProblems(&sub1, &sub2, aa, &subComplete);
+                changeMax(&subComplete, &max_energy[i][j]);
             }
-
-
-
-
         }
-
-
-
-
     }
 
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
+    printf("%lld\n", max_energy[1][n].totalEnergy);
+    printf("%s\n", max_energy[1][n].numList.c_str());
     return 0;
 }
